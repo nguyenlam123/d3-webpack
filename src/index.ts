ad45e1469eca754996430b52bf6956ec39514682
Nguyen Lam <nguyen.lam@tradeshift.com>
2021-12-01 11:26:56 +0100

    test: rest operator

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,7 @@ import * as d3 from 'd3';
 const select = d3.selectAll("div");
 
 console.log('select: ', select);
+
+let { x, y, ...z } = { x: 1, y: 2, a: 3, b: 4 };
+let n = { x, y, ...z };
+console.log(n); // { x: 1, y: 2, a: 3, b: 4 }
